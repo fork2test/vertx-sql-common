c87d0633ffddf1e8a62bf8c163117d051ed75274
fiorenzo pizza <fiorenzino@gmail.com>
2017-04-16 23:02:13 +0200

    3 new methods: persist, merge, delete

--- src/main/java/io/vertx/ext/sql/SQLConnection.java
+++ src/main/java/io/vertx/ext/sql/SQLConnection.java
@@ -22,6 +22,7 @@ import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 import java.util.List;
 
@@ -263,4 +264,45 @@ public interface SQLConnection extends AutoCloseable {
   default <N> N unwrap() {
     return null;
   }
+
+  /**
+   * Executes the given prepared statement which may be an <code>INSERT</code>
+   * statement with the given parameters
+   *
+   * @param table         the table to execute.
+   * @param params        these are the parameters name with values to fill the statement.
+   * @param resultHandler the handler which is called once the operation completes.
+   * @see java.sql.Statement#executeUpdate(String)
+   * @see java.sql.PreparedStatement#executeUpdate(String)
+   */
+  @Fluent
+  SQLConnection persist(String table, JsonObject params, Handler<AsyncResult<UpdateResult>> resultHandler);
+
+
+  /**
+   * Executes the given prepared statement which may be an <code>UPDATE</code>
+   * statement with the given parameters
+   *
+   * @param table         the table to execute.
+   * @param params        these are the parameters to fill the statement.
+   * @param key           name and value of table key.
+   * @param resultHandler the handler which is called once the operation completes.
+   * @see java.sql.Statement#executeUpdate(String)
+   * @see java.sql.PreparedStatement#executeUpdate(String)
+   */
+  @Fluent
+  SQLConnection merge(String table, JsonObject params, JsonObject key, Handler<AsyncResult<UpdateResult>> resultHandler);
+
+  /**
+   * Executes the given prepared statement which may be an <code>UPDATE</code>
+   * statement with the given parameters
+   *
+   * @param table         the table to execute.
+   * @param key           name and value of table key.
+   * @param resultHandler the handler which is called once the operation completes.
+   * @see java.sql.Statement#executeUpdate(String)
+   * @see java.sql.PreparedStatement#executeUpdate(String)
+   */
+  @Fluent
+  SQLConnection delete(String table, JsonObject key, Handler<AsyncResult<UpdateResult>> resultHandler);
 }
